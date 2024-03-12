9b0292938f37b88cd44c929ccba3756714fe1647
harper71 <princeazuka99@gmail.com>
2024-03-13 00:26:46 +0100

    correection commit

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
-def islower(c):
-    if c >= 'a' and c <= 'z':
-        return True
-    else:
-        return False
+def uppercase(s):
+    for char in s:
+        if ord('a') <= ord(char) <= ord('z'):
+            uppercase_char = chr(ord(char) - 32)
+        else:
+            uppercase_char = char
+        print("{}".format(uppercase_char), end='')
+    print()
+uppercase("Hello, world!")
