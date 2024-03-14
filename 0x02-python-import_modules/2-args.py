f795ae679e72d6355b1f19ecf2f13e7c1b3ec84e
harper71 <princeazuka99@gmail.com>
2024-03-14 18:59:18 +0100

    commit on task 0-2(6)

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 import sys
-
-values = len(sys.argv) - 1
-if values == 0:
-    print(f"{values}: arguments")
-else:
-    print(f"{values} arguments:")
-    for i in range(1, values + 1):
-        names = sys.argv[i]
-        print(f"{i}: {names}")
+if __name__ == "__main__":
+    values = len(sys.argv) - 1
+    if values == 0:
+        print("{}: arguments".format(values))
+    else:
+        print("{} arguments:".format(values))
+        for i in range(1, values + 1):
+            names = sys.argv[i]
+            print("{}: {}".format(i, names))
