71ee64772b0a1b4d3774401b14d160359ebb1f68
harper71 <princeazuka99@gmail.com>
2024-03-14 01:36:02 +0100

    correction commit(7)

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-for i in range(ord('z'), ord('A') - 1, -1):
-    print(chr(i).lower() if i % 2 == 0 else chr(i).upper(), end="")
+for i in range(97, 123):
+    if i != 101 and i != 113:
+        print("{}".format(chr(i)), end="")
--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-for i in range(10):
+for i in range(0, 9):
     for j in range(1, 10):
-        print(i, end="")
-        print(j, end=", ")
+        if i != j and i < j:
+            print(f"{i}", end="")
+            if i == 8 and j == 9:
+                print(f"{j}", end="\n")
+            else:
+                print(f"{j}", end=", ")
