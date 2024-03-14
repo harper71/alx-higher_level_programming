38f6256197e094ec96469852fcaf735b98698945
harper71 <princeazuka99@gmail.com>
2024-03-14 19:34:29 +0100

    commit on task 3(1)

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    args = sys.argv[1:]
+
+    nums = [int(argc) for argc in args]
+
+    total_sum = 0
+
+    if len(args) > 1:
+        for i in nums:
+            total_sum += i
+            
+    print("{}".format(total_sum))
+
