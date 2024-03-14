3c51731d9f803074ab0874b5a71d4e94491c1cd5
harper71 <princeazuka99@gmail.com>
2024-03-14 20:02:37 +0100

    commit on task 3 and 5(2)

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -10,6 +10,5 @@ if __name__ == "__main__":
     if len(args) > 1:
         for i in nums:
             total_sum += i
-            
-    print("{}".format(total_sum))
 
+    print("{}".format(total_sum))
