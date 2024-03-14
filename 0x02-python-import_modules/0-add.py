40de9be2e1447f4f054a8ecc30d815281cf62b04
harper71 <princeazuka99@gmail.com>
2024-03-14 18:38:20 +0100

    commit on task 0-2(3)

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 from add_0 import add
-a = 1
-b = 2
+if __name__ == "__main__":
+    a = 1
+    b = 2
 
-add_num = add(a, b)
-print(f"{a} + {b} = {add_num}")
+    add_num = add(a, b)
+    print(f"{a} + {b} = {add_num}")
