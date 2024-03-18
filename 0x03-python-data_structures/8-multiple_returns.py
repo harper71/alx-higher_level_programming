0c87caa8a54b86151e7ad3e4d6cf8ce9aa58882b
harper71 <princeazuka99@gmail.com>
2024-03-18 21:01:31 +0100

    commit(4) on task 7-12

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    string = tuple(sentence)
     amount = len(sentence)
     first_character = sentence[0] if amount > 0 else "None"
-    return amount, first_char
+    return amount, first_character
