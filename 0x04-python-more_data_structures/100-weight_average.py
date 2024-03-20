630d53dac1a28592da5e5bb91fd21b504b307439
harper71 <princeazuka99@gmail.com>
2024-03-20 21:09:26 +0100

    commit(2) on task 5-15

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,7 @@
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+    sum_products = sum(score * weight for score, weight in my_list)
+    sum_weights = sum(weight for _, weight in my_list)
+    weighted_avg = sum_products / sum_weights
+    return weighted_avg
---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    dictionary_copy = a_dictionary.copy()
+    for key, val in dictionary_copy.items():
+        if val == value:
+            del a_dictionary[key]
+    return a_dictionary
