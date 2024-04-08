0b2493dba1e8241d3543e5bc868279369e1cf156
harper71 <princeazuka99@gmail.com>
2024-04-08 22:19:26 +0100

    corrections to task 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -12,8 +12,37 @@ class Rectangle:
             width (int): width of rectangle. Defaults to 0.
             height (int): height of rectangle. Defaults to 0.
         """
-        self.height = height
-        self.width = width
+        self.__validate_size(height)
+        self.__validate_size2(width)
+
+        self.__height = height
+        self.__width = width
+
+    def __validate_size(self, height):
+        """Validates the size of the rectangle.
+
+        Raises:
+            ValueError: height must be >= 0
+            TypeError: height must be an integer
+        """
+
+        if type(height) not in [int]:
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+
+    def __validate_size2(self, width):
+        """Validates the size of the rectangle.
+
+        Raises:
+            ValueError: width must be >= 0
+            TypeError: width must be an integer
+        """
+
+        if type(width) not in [int]:
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
 
     @property
     def height(self):
