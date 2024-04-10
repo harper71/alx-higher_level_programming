388f9ee99c4504b437b35dfcfe82005cc67fadc9
harper71 <princeazuka99@gmail.com>
2024-04-10 01:27:01 +0100

    advanced

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
+"""prints the string * a set of times"""
+
+
 def magic_string():
+    """ duplicates a string"""
     return "".join(["BestSchool" + str(i) for i in range(1, n + 1)])
--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
+""" create a class"""
+
+
 class LockedClass:
+    """create a object """
     __slots__ = ['first_name']
