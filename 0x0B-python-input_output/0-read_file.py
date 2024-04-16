6371e3e3a204b579ab0e11942bc29671d14d4584
harper71 <princeazuka99@gmail.com>
2024-04-16 23:47:23 +0100

    task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -8,6 +8,6 @@ def read_file(filename=""):
     Args:
         filename (str): name of the file. Defaults to "".
     """
-    with open(filename, encoding="utf-8") as file:
+    with open(filename, 'r', encoding="utf-8") as file:
         f_contents = file.read()
         print(f_contents)
