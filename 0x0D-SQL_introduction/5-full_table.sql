74ed36421a26ec0f14bbacaaee94f310293dadc1
harper71 <princeazuka99@gmail.com>
2024-05-23 19:28:37 +0100

    table5

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,2 +1,12 @@
 -- shows the discription of full table
-SHOW first_table;
+SELECT 
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_KEY,
+    COLUMN_DEFAULT,
+    EXTRA
+FROM 
+    information_schema.COLUMNS
+WHERE 
+    TABLE_NAME = 'first_table';
