5c66af86b0fb3d70afe698264b376b6fa68e4afc
harper71 <princeazuka99@gmail.com>
2024-05-25 23:29:17 +0100

    task 1 and 0

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,3 @@
 -- grant all previleage to user
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
-FLUSH PRIVILEGES;
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
