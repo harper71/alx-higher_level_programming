ecafe8c727b79c886baece4584c7c56fa91a471c
harper71 <princeazuka99@gmail.com>
2024-06-21 06:25:39 +0100

    commit changes 2

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c !== undefined) {
+      for (let i = 0; i < this.height; i++) {
+        let row = '';
+        for (let j = 0; j < this.width; j++) {
+          row += c;
+        }
+        console.log(row);
+      }
+    } else if (c === undefined) {
+      for (let i = 0; i < this.height; i++) {
+        let row = '';
+        for (let j = 0; j < this.width; j++) {
+          row += 'X';
+        }
+        console.log(row);
+      }
+    }
+  }
+}
+module.exports = Square;
