93e594ef06678144ee1088e46f7fca59fddeb7ff
harper71 <princeazuka99@gmail.com>
2024-07-04 21:21:16 +0100

    git push task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
 # send a request to URL
-
-if [ -z "$1" ];
-then
-    echo "usage: $0 <URL>"
-    exit 1
-fi
-
-URL="$1"
-
-response=$(curl -s -o /dev/null -w '%{size_download}' "$URL")
-
-echo "$response"
+response=$(curl -s -o /dev/null -w '%{size_download}' "$1") && echo "$response"
