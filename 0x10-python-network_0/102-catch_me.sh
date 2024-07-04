d7359d6b62905847f50ca608dc0b6b038b1fe602
harper71 <princeazuka99@gmail.com>
2024-07-04 14:42:36 -0700

    adv

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # catch an error an prints "You got me!"
-curl -s --location --request PUT "http://0.0.0.0:5000/catch_me" --data ""
+curl -sL -X PUT -H "Origin: School" -d "user_id=98" 0.0.0.0:5000/catch_me
+
