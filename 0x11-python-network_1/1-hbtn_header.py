ea35755b36cd7a2400497a8c90dc43dde315fcc0
harper71 <princeazuka99@gmail.com>
2024-07-05 22:37:55 +0100

    commit changes

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -7,6 +7,20 @@ gets heade value
 
 URL = sys.argv[1]
 
-with urllib.request.urlopen(URL) as response:
-    head_value = response.getheader('X-Request-Id')
-    print(head_value)
+
+def fetch_request_id(url):
+    with urllib.request.urlopen(url) as response:
+        headers = response.getheaders()
+        request_id = dict(headers).get('X-Request-Id')
+        if request_id:
+            print(request_id)
+        else:
+            print('X-Request-Id header not found')
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <URL>")
+    else:
+        url = sys.argv[1]
+        fetch_request_id(url)
